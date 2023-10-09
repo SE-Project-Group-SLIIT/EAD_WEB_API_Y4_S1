1ec52226d8ebe1f9575f8b866a2dfe69552c050a
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2023-10-10 04:26:38 +0530

    change the program.cs

--- EAD_WEB_API_Y4_S1/Program.cs
+++ EAD_WEB_API_Y4_S1/Program.cs
@@ -27,11 +27,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", builder =>
+    {
+        builder
+            .WithOrigins("http://localhost:3000", "http://localhost:3001") // Add multiple origins
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -43,4 +55,22 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.Run();*/
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Add CORS middleware here
+app.UseCors("AllowSpecificOrigin");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
 app.Run();
+
