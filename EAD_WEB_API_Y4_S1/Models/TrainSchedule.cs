f5bb79223b7aadd7a6a906bfc7f39eafe297904e
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2023-10-12 00:48:28 +0530

    implement train schedule model

---
+++ EAD_WEB_API_Y4_S1/Models/TrainSchedule.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace EAD_WEB_API_Y4_S1.Models
+{
+    public class TrainSchedule
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? TrainScheduleId { get; set; }
+
+        [BsonElement("trainName")]
+        public string TrainName { get; set; } = null!;
+
+        [BsonElement("departureStation")]
+        public string DepartureStation { get; set; } = null!;
+
+        [BsonElement("arrivalStation")]
+        public string ArrivalStation { get; set; } = null!;
+
+        [BsonElement("trainType")]
+        public string TrainType { get; set; } = null!;
+
+        [BsonElement("departureTime")]
+        public DateTime DepartureTime { get; set; }
+
+        [BsonElement("arrivalTime")]
+        public DateTime ArrivalTime { get; set; }
+
+        [BsonElement("isActive")]
+        public bool IsActive { get; set; }
+
+        [BsonElement("isPublished")]
+        public bool IsPublished { get; set; }
+
+    }
+}
