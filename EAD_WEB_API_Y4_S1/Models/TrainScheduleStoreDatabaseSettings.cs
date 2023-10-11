b69b0ae7b7ed517ad74c4fd5d920e451812d3581
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2023-10-12 00:49:01 +0530

    implement train schedule store database

---
+++ EAD_WEB_API_Y4_S1/Models/TrainScheduleStoreDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace EAD_WEB_API_Y4_S1.Models
+{
+    public class TrainScheduleStoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string TrainScheduleCollectionName { get; set; } = null!;
+    }
+}
